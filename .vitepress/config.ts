a22e24c20d0423a957ec1f4d565a5566f445ddba
skanehira <sho19921005@gmail.com>
2023-06-04 23:48:39 +0900

    chore: add prettier

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vitepress'
-import { DefaultTheme } from 'vitepress/types/default-theme';
-import fs from 'node:fs'
+import { defineConfig } from "vitepress";
+import { DefaultTheme } from "vitepress/types/default-theme";
+import fs from "node:fs";
 
 const mdPattern = /\.md$/;
 function isMd(file: string) {
@@ -10,60 +10,67 @@ function isMd(file: string) {
 function subItems(dir: string): DefaultTheme.SidebarItem[] {
   const entries = fs.readdirSync(dir).filter((file) => {
     const stat = fs.statSync(dir);
-    return (stat.isFile() && isMd(file)) || stat.isDirectory()
+    return (stat.isFile() && isMd(file)) || stat.isDirectory();
   });
 
   const items: DefaultTheme.SidebarItem[] = [];
   for (const entry of entries) {
-    if (entry == 'index.md') {
-      continue
+    if (entry == "index.md") {
+      continue;
     }
     const path = `${dir}/${entry}`;
     const stat = fs.statSync(path);
     if (stat.isDirectory()) {
-      const item: DefaultTheme.SidebarItem = { text: entry, link: `/${path}/index.md`, items: [] }
-      item.items = subItems(path)
-      items.push(item)
+      const item: DefaultTheme.SidebarItem = {
+        text: entry,
+        link: `/${path}/index.md`,
+        items: [],
+      };
+      item.items = subItems(path);
+      items.push(item);
     } else {
-      const item = { text: entry.replace(mdPattern, ''), link: `/${path}` }
-      items.push(item)
+      const item = { text: entry.replace(mdPattern, ""), link: `/${path}` };
+      items.push(item);
     }
   }
 
-  return items
+  return items;
 }
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/this-week-in-gorilla/',
+  base: "/this-week-in-gorilla/",
   title: "This Week in Gorilla",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Docs', link: '/docs/2023/index' },
-      { text: 'Books', link: '/books/index' },
+      { text: "Home", link: "/" },
+      { text: "Docs", link: "/docs/2023/index" },
+      { text: "Books", link: "/books/index" },
     ],
 
     sidebar: [
       {
-        text: 'docs',
-        items: subItems('docs')
+        text: "docs",
+        items: subItems("docs"),
       },
       {
-        text: 'books',
-        link: '/books/index.md',
-        items: subItems('books')
-      }
+        text: "books",
+        link: "/books/index.md",
+        items: subItems("books"),
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/skanehira/this-week-in-gorilla' },
-      { icon: 'twitter', link: 'https://twitter.com/gorilla0513' }
+      {
+        icon: "github",
+        link: "https://github.com/skanehira/this-week-in-gorilla",
+      },
+      { icon: "twitter", link: "https://twitter.com/gorilla0513" },
     ],
 
     search: {
-      provider: 'local'
-    }
-  }
-})
+      provider: "local",
+    },
+  },
+});
